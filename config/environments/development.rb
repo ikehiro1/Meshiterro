028d3c5caadcba1505de5a6599e88bb8938a589d
EC2 Default User <ec2-user@ip-172-31-18-173.ap-southeast-2.compute.internal>
2023-08-22 15:30:51 +0000

    5-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,3 @@
-config.active_job.queue_adapter = :inline
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -74,5 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts.clear
+  config.active_job.queue_adapter = :inline
+  
+ config.hosts.clear
 end
