c416fd8e6023f89058e28f63fba45bf7c900db7e
EC2 Default User <ec2-user@ip-172-31-18-173.ap-southeast-2.compute.internal>
2023-08-24 07:59:29 +0000

    4-10finish

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,12 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
   end
 
   def index
@@ -7,4 +14,9 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  private
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   
-  resources :post_image, onlry: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   get "homes/about" => "homes#about"
 end
